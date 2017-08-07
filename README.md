abb80c5a34ff7ae2187c774afd309e684f0d0f5d
Ryan Hamblin <ryanhamblin@Ryans-MBP.domain>
2017-08-07 13:10:25 -0600

    initial

---
+++ README.md
@@ -0,0 +1,35 @@
+# Testing - Mini
+
+Topics:
+  * Mocha
+  * Chai
+  * Sinon
+  * Assert
+  * Expect
+
+
+## Project Description
+
+* The objective is to get you exposed to using TDD with Mocha/Chai and Sinon.
+* You will be writing tests for the code inside of `src/app.js`.
+
+### Initialize Project
+
+* cd into repo and run `npm install`
+
+### Pre Set Up
+
+* You'll notice we have two directories `src` and `test`. Inside of `src/app.js` you'll see all the code that we want to test, being exported out of `app.js` in the bottom of the file.
+* Go ahead and run `npm test` and it will exectute the testing suites.
+
+### Steps for implementing Tests
+
+* Step 1: You have a broken test suite already, go ahead and change each test assertion such that it passes the expected test. Everything begins on line 14 inside of `tests/app.test.js` begin there.
+
+### Resources
+
+* [Mocha](https://mochajs.org/)
+* [sinon-spy](https://github.com/domenic/sinon-chai)
+* [Chai](http://chaijs.com/)
+* [Assert](http://chaijs.com/api/assert/)
+* [Expect](http://chaijs.com/api/bdd/)
---
+++ src/app.js
@@ -0,0 +1,67 @@
+const apples = ['Gala', 'Red Delicious', 'Fuji', 'Cortland'];
+
+const Ben = {
+  favBand: 'Beastie Boys',
+  favFood: 'Mexican',
+  favBook: 'Catcher in the Rye',
+  favColor: 'Blue Green',
+  favActivity: 'Coding',
+};
+
+const Sean = {
+  favBand: 'Metallica',
+  favFood: 'Pizza',
+  favBook: 'Life of Pi',
+  favColor: 'Green',
+  favActivity: 'Surfing',
+};
+
+const Ryan = {
+  favBand: 'Guster',
+  favFood: 'Chinese',
+  favBook: 'The Giver',
+  favColor: 'Silver',
+  favActivity: 'Mountain Biking',
+};
+
+const Austen = {
+  favBand: 'Third Eye Blind',
+  favFood: 'Middle Eastern',
+  favBook: '50 Shades of Grey',
+  favColor: 'Yellow',
+  favActivity: 'Comedy Clubs',
+};
+
+const Karthik = {
+  favBand: 'Smashing Pumpkins',
+  favFood: 'Spaghetti',
+  favBook: 'The Puppy who Lost His Way',
+  favColor: 'Red',
+  favActivity: 'Rock Climbing',
+};
+
+const addNums = (x, y) => {
+  return x + y;
+};
+
+const callBackInvoker = (cb) => {
+  return (cb());
+};
+
+const iterator = (num, cb) => {
+  for (let i = 1; i <= num.length; i++) {
+    cb();
+  }
+};
+
+module.exports = {
+  apples,
+  Ben,
+  Sean,
+  Ryan,
+  Austen,
+  Karthik,
+  addNums,
+  callBackInvoker,
+  iterator,
+};
