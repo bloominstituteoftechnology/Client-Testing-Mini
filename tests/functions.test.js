ddd661775ef7eeabb0250e3a7d4074f03108a996
JacquelynnRohrich <33401573+JacquelynnRohrich@users.noreply.github.com>
2018-01-29 12:58:13 -0600

    Finished passing all tests

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -7,28 +7,20 @@ const assert = chai.assert;
 chai.use(sinonChai);
 
 describe('functions', () => {
-  let testArray = [];
-  beforeEach = () => {
-    testArray = [1, 2, 3, 4, 'eggs', 'cheese', 'milk'];
-  };
-
-  afterEach = () => {
-    testArray = [];
-  };
-
   describe('addNums', () => {
     const addNums = cases.addNums;
     it('should be a function', () => {
       expect(addNums).to.be.a('function');
     });
-    // write a test to check if 'addNums' returns the expected value
-    // i.e. if is called addNums(1, 2); the return value should be 3.
+    it('should return the proper output given specific input', () => {
+      expect(addNums(1, 2)).to.equal(3);
+    });
   });
 
   describe('callBackInvoker', () => {
     it('should be a function', () => {
       const callBackInvoker = cases.callBackInvoker;
-      expect(callBackInvoker).to.be.a('number');
+      expect(callBackInvoker).to.be.a('function');
     });
     it('should invoke a given callback passed to it', () => {
       // this is where you're going to be using 'chai's sinon' spy function.
@@ -43,9 +35,23 @@ describe('functions', () => {
   describe('iterator', () => {
     it('should be a function', () => {
       const iterator = cases.iterator;
-      expect(iterator).to.be.a('string');
+      expect(iterator).to.be.a('function');
     });
     //similiar to above where we are utilizing our spy from sinon, this assertion should test if a cb is called x times.
     it('should call a callback for n times passed to cases.iterator', () => {});
+  });
+
+  describe('each', () => {
+    let testArray = [];
+    beforeEach = () => {
+      testArray = [1, 2, 3, 4, 'eggs', 'cheese', 'milk'];
+    };
+
+    afterEach = () => {
+      testArray = [];
+    };
+    it('skiiper', () => {
+      console.log(testArray);
+    });
   });
 });
--- tests/types.test.js
+++ tests/types.test.js
@@ -9,40 +9,67 @@ describe('Cases from app.js', () => {
   describe('apples', () => {
     it('should be an array', () => {
       const apples = cases.apples;
-      expect(apples).to.be.an('object'); // Begin here something is wrong, fix it.
+      expect(apples).to.be.an('array'); // Begin here something is wrong, fix it.
     });
     // write a test to check the correct length of the apples array.
     // write a test to check to see if the apples array contains 'Red Delicious'.
+    it('should have length of 4', () => {
+      const apprs = cases.apples;
+      expect(apprs).to.have.lengthOf(4);
+    });
+
+    it('should contain the string `Red Delicious`', () => {
+      const apples = cases.apples;
+      expect(apples).to.include('Red Delicious');
+    });
   });
 
   describe('Ben', () => {
     it('should be an object', () => {
       const Ben = cases.Ben;
-      expect(Ben).to.be.an('array');
+      expect(Ben).to.be.an('object');
     });
     // write a test to check if Ben's favorite band is 'Beastie Boys'.
+    it('should have prop favBand equal to `Beastie Boys`', () => {
+      const favBand = cases.Ben.favBand;
+      assert.typeOf(favBand, 'string');
+      expect(favBand).to.equal('Beastie Boys', 'Ben\'s favorite band is the Beastie Boys');
+      assert.equal(favBand, 'Beastie Boys', 'Ben\'s favorite band is the Beastie Boys');
+    });
   });
 
   describe('Sean', () => {
     it('should be an object', () => {
       const Sean = cases.Sean;
-      expect(Sean).to.be.an('number');
+      expect(Sean).to.be.an('object');
     });
     // write a test to check if Seans's favorite food is 'Pizza'.
+    it('should have prop favFood equal to `Pizza`', () => {
+      const favFood = cases.Sean.favFood;
+      assert.typeOf(favFood, 'string');
+      expect(favFood).to.equal('Pizza', 'Sean\'s favorite food is Pizza');
+      assert.equal(favFood, 'Pizza', 'Sean\'s favorite food is Pizza');
+    });
   });
 
   describe('Ryan', () => {
     it('should be an object', () => {
       const Ryan = cases.Ryan;
-      expect(Ryan).to.be.an('string');
+      expect(Ryan).to.be.a('object');
     });
     // write a test to see if Ryan's favBook is `not` 'Harry Potter'.
+    it('should have prop favBook is not equal to `Harry Potter`', () => {
+      const favBook = cases.Ryan.favBook;
+      assert.typeOf(favBook, 'string');
+      expect(favBook).to.not.equal('Harry Potter', 'Ryan\'s favorite book is not Harry Potter');
+      //assert.not.equal(favBook, 'Harry Potter', 'Ryan\'s favorite book is not Harry Potter');
+    });
   });
 
   describe('Austen', () => {
     it('should be an object', () => {
       const Austen = cases.Austen;
-      expect(Austen).to.be.an('function');
+      expect(Austen).to.be.an('object');
     });
     // write a test to see if Austen's favColor is not 'Gold'.
   });
@@ -50,7 +77,7 @@ describe('Cases from app.js', () => {
   describe('Ivan', () => {
     it('should be an object', () => {
       const Ivan = cases.Ivan;
-      expect(Ivan).to.be.an('function');
+      expect(Ivan).to.be.an('object');
     });
     // write a test to check if Ivan's favActivity is 'Rock Climbing'.
   });
