523d26a2395388823c4f88379529ac93ceb9ab0e
sarika <sarikaresume@gmail.com>
2018-01-29 10:34:25 -0800

    apples

--- tests/types.test.js
+++ tests/types.test.js
@@ -9,19 +9,33 @@ describe('Cases from app.js', () => {
   describe('apples', () => {
     it('should be an array', () => {
       const apples = cases.apples;
-      expect(apples).to.be.an('object'); // Begin here something is wrong, fix it.
+      expect(apples).to.be.an('array'); // Begin here something is wrong, fix it.
     });
     // write a test to check the correct length of the apples array.
+  describe('apples', () => {
+    it('should be length four', () => {
+      const apples = cases.apples;
+      expect(apples.length).to.equal(4);
+    }
     // write a test to check to see if the apples array contains 'Red Delicious'.
+  describe('apples', () => {
+    it('should contain Red Delicious', () => {
+      const apples = cases.apples; 
+      expect(apples).to.include('RedDelicious', true);
   });
 
   describe('Ben', () => {
     it('should be an object', () => {
       const Ben = cases.Ben;
-      expect(Ben).to.be.an('array');
+      expect(Ben).to.be.an('object');
     });
     // write a test to check if Ben's favorite band is 'Beastie Boys'.
   });
+  describe('Ben', () => {
+    it('fav band is', () => {
+      const Ben = cases.Ben; 
+      expect(Ben.favBand).to.equal('Beastie Boys');
+    });
 
   describe('Sean', () => {
     it('should be an object', () => {
