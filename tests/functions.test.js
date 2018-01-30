3b26d13a2afce095bf2fcf7af7b58c4154b9516d
sarika <sarikaresume@gmail.com>
2018-01-30 13:21:40 -0800

    hello

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -6,46 +6,54 @@ const expect = chai.expect;
 const assert = chai.assert;
 chai.use(sinonChai);
 
-  describe('each', () => {
+  describe('functions', () => {
   let testArray = [];
   beforeEach = () => {
     testArray = [1, 2, 3, 4, 'eggs', 'cheese', 'milk'];
-  });
+  };
 
   afterEach = () => {
     testArray = [];
   };
 
   describe('addNums', () => {
-    const addNums = cases.addNums;
     it('should be a function', () => {
+      const addNums = cases.addNums;
       expect(addNums).to.be.a('function');
     });
-    
-  describe('addNums(1, 2)', () => {
-    const addNums = cases.addNums(1, 2); 
-    it('should return the proper output', () => {
-      expect(addNums(1, 2)).to.be.a('function returning 3');
+    it('should add up to two numbers passed to it', () => {
+    const result = cases.addNums(1, 2); 
+    expect(result).to.equal(3);
 });
+  });
 
   describe('callBackInvoker', () => {
     it('should be a function', () => {
       const callBackInvoker = cases.callBackInvoker;
-      expect(callBackInvoker).to.be.a('number');
-    });
-    it('should invoke a given callback passed to it', () => {
-      const callBack = sinon.spy();
-      expect (sinon.spy).to.be.a('newCbInvoker');
+      expect(callBackInvoker).to.be.a('function');
     });
 
+    it('should invoke a given callback passed to it', () => {
+      const callBack = sinon.spy();
+      const newCbInvoker = cases.callBackInvoker;
+      newCbInvoker(callBack);
+      expect (callBack).to.have.been.calledOnce;
+    });
+
+  });
+
   describe('iterator', () => {
+
     it('should be a function', () => {
       const iterator = cases.iterator;
       expect(iterator).to.be.a('function');
     });
  
     it('should call a callback for n times passed to cases.iterator', () => {
-      const callBack = cases.iterator; 
-      expect(callback).to.be.a('callback called x times')
+      const callBack = sinon.spy; 
+      const iterator = cases.iterator;
+      iterator(4, callBack);
+      expect(callBack).to.have.callCount(4);
   });
+});
 });
--- tests/types.test.js
+++ tests/types.test.js
@@ -15,14 +15,15 @@ describe('Cases from app.js', () => {
   describe('apples', () => {
     it('should be length four', () => {
       const apples = cases.apples;
-      expect(apples.length).to.equal(4);
-    }
+      expect(apples).have.lengthOf(4);
+    });
     // write a test to check to see if the apples array contains 'Red Delicious'.
   describe('apples', () => {
     it('should contain Red Delicious', () => {
       const apples = cases.apples; 
-      expect(apples).to.include('RedDelicious', true);
+      expect(apples).to.include('RedDelicious');
   });
+});
 
   describe('Ben', () => {
     it('should be an object', () => {
@@ -32,59 +33,69 @@ describe('Cases from app.js', () => {
     // write a test to check if Ben's favorite band is 'Beastie Boys'.
   });
   describe('Ben', () => {
-    it('fav band is', () => {
-      const Ben = cases.Ben; 
-      expect(Ben.favBand).to.equal('Beastie Boys');
+    it('should have fav band is equal to Beastie Boys', () => {
+      const favBand = cases.Ben.favBand; 
+      assert.typeOf(favBand, 'string');
+      expect(favBand).to.equal('Beastie Boys', 'Ben\'s favorite band');
+      assert.equal(favBand, 'Beastie Boys', "Ben\'s favorite band is the Beastie Boys");
     });
+  });
 
   describe('Sean', () => {
     it('should be an object', () => {
       const Sean = cases.Sean;
-      expect(Sean).to.be.an('number');
+      expect(Sean).to.be.an('object');
     });
     // write a test to check if Seans's favorite food is 'Pizza'.
   describe('Sean', () => {
-      it('favorite food should be Pizza', () => {
-        const Sean = cases.Sean;
-        expect(Sean).to.be.equal.toString('favorite food should be Pizza');
+      it('Sean\'s favorite food should be Pizza', () => {
+        const favFood = cases.Sean.favFood;
+        assert.typeOf(favFood, 'string');
+        expect(favFood).to.equal('Sean\'s favorite food should be Pizza');
       });
+    });
 
   describe('Ryan', () => {
     it('should be an object', () => {
       const Ryan = cases.Ryan;
-      expect(Ryan).to.be.an('function');
+      expect(Ryan).to.be.an('object');
     });
     // write a test to see if Ryan's favBook is `not` 'Harry Potter'.
   });
 
-  describe('Ryan', () => {
     it('should be Ryans favBook is not Harry Potter', () => {
-      const Ryan = cases.Ryan;
-      expect(Ryan).to.be.an('Ryans favBook is not Harry Potter');
+      const favBook = cases.Ryan.favBook;
+      assert.typeOf(favBook, 'string');
+      expect(favBook).to.not.equal('Ryans favBook is not Harry Potter');
     });
+  });
+});
 
   describe('Austen', () => {
     it('should be an object', () => {
       const Austen = cases.Austen;
-      expect(Austen).to.be.an('function');
+      expect(Austen).to.be.an('object');
     });
     // write a test to see if Austen's favColor is not 'Gold'.
   });
-  describe('Austen', () => {
     it('favColor is not Gold', () => {
-      const Austen = cases.Austen;
-      expect(Austen).to.be.an('favColor is not Gold');
+      const favColor = cases.Austen.favColor;
+      assert.typeOf(favColor, 'string');
+      expect(favColor).to.be.not.equal('Gold', 'Austen\'s favColor is not Gold');
     });
+  });
 
   describe('Ivan', () => {
     it('should be an object', () => {
       const Ivan = cases.Ivan;
-      expect(Ivan).to.be.an('function');
+      expect(Ivan).to.be.an('object');
     });
     // write a test to check if Ivan's favActivity is 'Rock Climbing'.
   });
-  describe('Ivan', () => {
     it('favActivity is Rock Climbing', () => {
-      const Ivan = cases.Ivan;
-      expect(Ivan).to.be.an('favActivity is Rock Climbing');
+      const favActivity = cases.Ivan.favActivity;
+      assert.typeOf(favActivity, 'string');
+      expect(favActivity).to.equal('Rock Climbing', 'Ivan\'s favActivity is Rock Climbing');
+      assert.equal(favActivity, 'Rock Climbing', 'Ivan\'s favorite activity is Rock Climbing');
     });
+  });
