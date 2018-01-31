afe8160badff60e2fbfe61fd14216047bf907d1a
Cole Phillips <nphillips78@gmail.com>
2018-01-30 19:07:02 -0500

    types completed

--- tests/types.test.js
+++ tests/types.test.js
@@ -1,27 +1,24 @@
 const chai = require('chai');
-const sinon = require('sinon');
-const sinonChai = require(sinon-chai)
 const cases = require('../src/app.js');
 const expect = chai.expect;
 const assert = chai.assert;
-chai.use(sinonChai);
 
 describe('Cases from app.js', () => {
   // this is a testing suite made up of suites.
 
   describe('apples', () => {
+    const apples = cases.apples;
     it('should be an array', () => {
-      const apples = cases.apples;
       expect(apples).to.be.an('array'); // Begin here something is wrong, fix it.
     });
-    it('should have a length of 4', () => {
-      expect(apples).to.have.lengthOf.(4);
-    });
-    it('should contain the string `Red Delicious`', () => {
-      expect(apples).to.include.('Red Delicious');
-    });
     // write a test to check the correct length of the apples array.
+    it('should have length of 4', () => {
+      expect(apples.length).to.be.equal(4);
+    });
     // write a test to check to see if the apples array contains 'Red Delicious'.
+    it("should contain 'Red Delicious'", () => {
+      expect(apples.includes('Red Delicious')).to.be.true;
+    });
   });
 
   describe('Ben', () => {
@@ -32,9 +29,11 @@ describe('Cases from app.js', () => {
     it('should have prop favBand equal to `Beastie Boys`', () => {
       const favBand = cases.Ben.favBand;
       assert.typeOf(favBand, 'string');
-      expect(favBand).to.equal('Beastie Boys', 'Ben\s favorite band is the Beastie Boys');
+      expect(favBand).to.equal(
+        'Beastie Boys',
+        'Bens favorite band is the Beastie Boys'
+      );
     });
-    // write a test to check if Ben's favorite band is 'Beastie Boys'.
   });
 
   describe('Sean', () => {
@@ -45,46 +44,50 @@ describe('Cases from app.js', () => {
     it('should have prop favFood equal to `Pizza`', () => {
       const favFood = cases.Sean.favFood;
       assert.typeOf(favFood, 'string');
-      expect(favFood).to.equal('Pizza', 'Sean\'s favorite food is Pizza');
+      expect(favFood).to.equal('Pizza', "Sean's favorite food is Pizza");
     });
-    // write a test to check if Seans's favorite food is 'Pizza'.
- 
 
-  describe('Ryan', () => {
-    it('should be an object', () => {
-      const Ryan = cases.Ryan;
-      expect(Ryan).to.be.an('object');
+    describe('Ryan', () => {
+      it('should be an object', () => {
+        const Ryan = cases.Ryan;
+        expect(Ryan).to.be.an('object');
+      });
+      it('should have prop favBook not equal to `Harry Potter`', () => {
+        const favBook = cases.Ryan.favBook;
+        assert.typeOf(favBook, 'string');
+        expect(favBook).to.not.equal(
+          'Harry Potter',
+          "Ryan's favorite book is not Harry Potter"
+        );
+      });
+      // write a test to see if Ryan's favBook is `not` 'Harry Potter'.
     });
-    it('should have prop favBook not equal to `Harry Potter`', () => {
-      const favBook = cases.Ryan.favBook;
-      assert.typeOf(favBook, 'string');
-      expect(favBook).to.not.equal('Harry Potter', 'Ryan\'s favorite book is not Harry Potter');
-    });
-    // write a test to see if Ryan's favBook is `not` 'Harry Potter'.
-  });
 
-  describe('Austen', () => {
-    it('should be an object', () => {
-      const Austen = cases.Austen.favColor;
-      expect(Austen).to.be.an('object');
+    describe('Austen', () => {
+      const Austen = cases.Austen;
+      it('should be an object', () => {
+        expect(Austen).to.be.an('object');
+      });
+      // write a test to see if Austen's favColor is not 'Gold'.
+      it("Austen's favorite color should equal 'Gold'", () => {
+        expect(Austen.favColor).not.to.be.equal('Gold');
+      });
     });
-    it('should have prop favColor not equal to `Gold`');
-    const favColor = cases.Austen.favColor;
-    assert.typeOf(favColor, 'string');
-    expect(favColor).to.not.equal('Gold', 'Austen\'s favorite color is not Gold');
-    // write a test to see if Austen's favColor is not 'Gold'.
-  });
 
-  describe('Ivan', () => {
-    it('should be an object', () => {
-      const Ivan = cases.Ivan;
-      expect(Ivan).to.be.an('object');
+    describe('Ivan', () => {
+      it('should be an object', () => {
+        const Ivan = cases.Ivan;
+        expect(Ivan).to.be.an('object');
+      });
+      it('should have prop favActivity equal to `Rock Climbing`');
+      const favActivity = cases.Ivan.favActivity;
+      assert.typeOf(favActivity, 'string');
+      expect(favActivity).to.equal(
+        'Rock Climbing',
+        "Ivan's favorite activity is Rock Climbing"
+      );
+
+      // write a test to check if Ivan's favActivity is 'Rock Climbing'.
     });
-    it('should have prop favActivity equal to `Rock Climbing`');
-    const favActivity = cases.Ivan.favActivity;
-    assert.typeOf(favActivity, 'string');
-    expect(favActivity).to.equal('Rock Climbing', 'Ivan\'s favorite activity is Rock Climbing');
-
-    // write a test to check if Ivan's favActivity is 'Rock Climbing'.
   });
-  });
+});
