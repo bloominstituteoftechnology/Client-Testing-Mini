80ae40e24f92c89e77b5849ca2249f6c28155fc7
Your Name <haywood.d.johnson@gmail.com>
2018-03-14 00:15:56 -0500

    finished!

--- tests/types.test.js
+++ tests/types.test.js
@@ -9,7 +9,7 @@ describe('Cases from app.js', () => {
   describe('apples', () => {
     it('should be an array', () => {
       const apples = cases.apples;
-      expect(apples).to.be.an('object'); // Begin here something is wrong, fix it.
+      expect(apples).to.be.an('array'); // Begin here something is wrong, fix it.
     });
     // write a test to check the correct length of the apples array.
     // write a test to check to see if the apples array contains 'Red Delicious'.
@@ -18,7 +18,7 @@ describe('Cases from app.js', () => {
   describe('Ben', () => {
     it('should be an object', () => {
       const Ben = cases.Ben;
-      expect(Ben).to.be.an('array');
+      expect(Ben).to.be.an('object');
     });
     // write a test to check if Ben's favorite band is 'Beastie Boys'.
   });
@@ -26,7 +26,7 @@ describe('Cases from app.js', () => {
   describe('Sean', () => {
     it('should be an object', () => {
       const Sean = cases.Sean;
-      expect(Sean).to.be.an('number');
+      expect(Sean).to.be.an('object');
     });
     // write a test to check if Seans's favorite food is 'Pizza'.
   });
@@ -34,7 +34,7 @@ describe('Cases from app.js', () => {
   describe('Ryan', () => {
     it('should be an object', () => {
       const Ryan = cases.Ryan;
-      expect(Ryan).to.be.an('string');
+      expect(Ryan).to.be.an('object');
     });
     // write a test to see if Ryan's favBook is `not` 'Harry Potter'.
   });
@@ -42,7 +42,7 @@ describe('Cases from app.js', () => {
   describe('Austen', () => {
     it('should be an object', () => {
       const Austen = cases.Austen;
-      expect(Austen).to.be.an('function');
+      expect(Austen).to.be.an('object');
     });
     // write a test to see if Austen's favColor is not 'Gold'.
   });
@@ -50,7 +50,7 @@ describe('Cases from app.js', () => {
   describe('Ivan', () => {
     it('should be an object', () => {
       const Ivan = cases.Ivan;
-      expect(Ivan).to.be.an('function');
+      expect(Ivan).to.be.an('object');
     });
     // write a test to check if Ivan's favActivity is 'Rock Climbing'.
   });
