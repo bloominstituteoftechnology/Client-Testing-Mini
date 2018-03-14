00e33b5dda0e72e80b724cd5628fca6dc81049cf
Elly <ellyhahn@gmail.com>
2018-03-13 19:50:49 -0700

    Elly Han - finally all tests were passed

--- tests/functions.test.js
+++ tests/functions.test.js
@@ -22,18 +22,25 @@ describe('functions', () => {
       expect(addNums).to.be.a('function');
     });
     // write a test to check if 'addNums' returns the expected value
+    it('should return the sum of two integers', () => {
+      const sum = addNums(1, 2);
+      expect(sum).to.equal(3)
+    })
     // i.e. if is called addNums(1, 2); the return value should be 3.
   });
 
   describe('callBackInvoker', () => {
     it('should be a function', () => {
       const callBackInvoker = cases.callBackInvoker;
-      expect(callBackInvoker).to.be.a('number');
+      expect(callBackInvoker).to.be.a('function');
     });
     it('should invoke a given callback passed to it', () => {
       // this is where you're going to be using 'chai's sinon' spy function.
       const callBack = sinon.spy();
       const newCbInvoker = cases.callBackInvoker;
+      newCbInvoker(callBack);
+      expect(callBack).to.have.been.called;
+      
       // pass our spy `callBack` to our newCbInvoker fn.
       // write a test that to see if our callback has been called.
       // hint - you will need to look at https://github.com/domenic/sinon-chai to see syntax around this
@@ -43,9 +50,18 @@ describe('functions', () => {
   describe('iterator', () => {
     it('should be a function', () => {
       const iterator = cases.iterator;
-      expect(iterator).to.be.a('string');
+      expect(iterator).to.be.a('function');
     });
     //similiar to above where we are utilizing our spy from sinon, this assertion should test if a cb is called x times.
-    it('should call a callback for n times passed to cases.iterator', () => {});
+    it('should call a callback for n times passed to cases.iterator', () => {
+      const callBack = sinon.spy();
+      const newIterator = cases.iterator;
+      var arrayOfN = [2, 5, 10, 100];
+      arrayOfN.forEach(n => {
+        callBack.callCount = 0;
+        newIterator(n, callBack);
+        expect(callBack.callCount).to.equal(n);
+      });
+    });
   });
 });
