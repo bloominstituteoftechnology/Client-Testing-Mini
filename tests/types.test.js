d764a1be59568064b61ba4e3dfbb9c1333820681
charletta <charlettabullard12@outlook.com>
2018-04-23 16:23:16 -0400

    Completed Testing Mini

--- tests/types.test.js
+++ tests/types.test.js
@@ -9,49 +9,78 @@ describe('Cases from app.js', () => {
   describe('apples', () => {
     it('should be an array', () => {
       const apples = cases.apples;
-      expect(apples).to.be.an('object'); // Begin here something is wrong, fix it.
+      expect(apples).to.be.an('array'); // Begin here something is wrong, fix it.
     });
     // write a test to check the correct length of the apples array.
+    it('should be an array and have apples.length', () => {
+      const apples = cases.apples;
+      expect(apples).to.have.lengthOf(4);
+    });
     // write a test to check to see if the apples array contains 'Red Delicious'.
+    // expect(array).to.contain('Red Delicious');
+    it('should return apples contain Red Delicious', () => {
+      const apples = cases.apples;
+      expect(apples).to.include('Red Delicious');
+    });
   });
 
   describe('Ben', () => {
     it('should be an object', () => {
       const Ben = cases.Ben;
-      expect(Ben).to.be.an('array');
+      expect(Ben).to.be.an('object');
     });
     // write a test to check if Ben's favorite band is 'Beastie Boys'.
+    it('should return favBand as Beastie Boys', () => {
+      const favBand = cases.Ben;
+      expect(favBand).to.include({ favBand: 'Beastie Boys' });
+    });
   });
 
   describe('Sean', () => {
     it('should be an object', () => {
       const Sean = cases.Sean;
-      expect(Sean).to.be.an('number');
+      expect(Sean).to.be.an('object');
     });
     // write a test to check if Seans's favorite food is 'Pizza'.
+    it('should return favFood as Pizza', () => {
+      const Sean = cases.Sean;
+      expect(Sean).to.include({ favFood: 'Pizza' });
+    });
   });
 
   describe('Ryan', () => {
     it('should be an object', () => {
       const Ryan = cases.Ryan;
-      expect(Ryan).to.be.an('string');
+      expect(Ryan).to.be.an('object');
     });
     // write a test to see if Ryan's favBook is `not` 'Harry Potter'.
+    it('should return favBook as Harry Potter', () => {
+      const Ryan = cases.Ryan;
+      expect(Ryan).to.not.include({ favBook: 'Harry Potter' });
+    });
   });
 
   describe('Austen', () => {
     it('should be an object', () => {
       const Austen = cases.Austen;
-      expect(Austen).to.be.an('function');
+      expect(Austen).to.be.an('object');
     });
     // write a test to see if Austen's favColor is not 'Gold'.
+    it('should return favColor not Gold', () => {
+      const Austen = cases.Austen;
+      expect(Austen).to.not.include({ favColor: 'Gold' });
+    });
   });
 
   describe('Ivan', () => {
     it('should be an object', () => {
       const Ivan = cases.Ivan;
-      expect(Ivan).to.be.an('function');
+      expect(Ivan).to.be.an('object');
     });
     // write a test to check if Ivan's favActivity is 'Rock Climbing'.
+    it('should return favActivity as Rock Climbing', () => {
+      const Ivan = cases.Ivan;
+      expect(Ivan).to.include({ favActivity: 'Rock Climbing' });
+    });
   });
 });
